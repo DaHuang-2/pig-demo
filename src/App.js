bdbf3712a116df4925537a1476c0b72db7d2e2d7
zengpen <1870698041@qq.com>
2019-11-08 11:23:42 +0800

    zp3冲突解决

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import './global.less'
-import City from './views/City'
-import Home from './views/Home'
 import HomeDetail from './views/HomeDetail'
-=======
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./global.less";
@@ -14,20 +7,16 @@ import Home from "./views/Home";
 import Login from "./views/Login";
 import Register from "./views/Register";
 import Order from "./views/Member/Order";
->>>>>>> d91ad67a7e3f6024c2dfbbd94e92528834ac0c56
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route path="/city" component={City}></Route>
-<<<<<<< HEAD
         <Route path="/detail" component={HomeDetail}></Route>
-=======
         <Route path="/login" component={Login}></Route>
         <Route path="/register" component={Register}></Route>
         <Route path="/order" component={Order}></Route>
->>>>>>> d91ad67a7e3f6024c2dfbbd94e92528834ac0c56
         <Route path="/" component={Home}></Route>
        
       </Switch>
