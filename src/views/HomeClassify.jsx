b65374498a83eea01e5eeef5d1ed963efdbf886b
zengpen <1870698041@qq.com>
2019-11-08 22:09:56 +0800

    详情页

--- src/views/HomeClassify.jsx
+++ src/views/HomeClassify.jsx
@@ -26,15 +26,9 @@ class HomeClassify extends React.Component {
             <span className='iconfont  icon-fanhui'></span>
           </div>
 
-<<<<<<< HEAD
           <div className="page-content "style={{ borderRadius:"20px",marginRight:"5px",padding:"0px 20px"}}>
             <div className="headerbar-content-left" >
               <i className="iconfont icon-sousuo"></i>
-=======
-          <div className='page-content'>
-            <div className='headerbar-content-left'>
-              <i className='iconfont icon-sousuo'></i>
->>>>>>> 3e8dd8b890de97bff5bf6373da6405869dbd313d
             </div>
             <div className='headerbar-content-center'>
               <input
@@ -131,7 +125,6 @@ const TabExample = props => {
                   <img src={item.pic02} alt="" style={{width:"56px",height:"56px",marginBottom:"10px",marginTop:"10px"}}/>
                   <p style={{fontSize: "16px",fontWeight:"300",}}>{item.title02}</p>
                   </div>
-<<<<<<< HEAD
                {/* 第三竖列 */}
                   <div className="zp-pic-i" style={{textAlign:"center",width:"33.3%",height:"80px",marginTop:"5px"}}>
                   <img src={item.pic02} alt="" style={{width:"56px",height:"56px",marginBottom:"10px",marginTop:"5px"}}/>
@@ -153,17 +146,6 @@ const TabExample = props => {
                  
                  
                  
-=======
-
-                  <div className="zp-pic-i" style={{textAlign:"center",width:"33.3%",height:"80px",marginTop:"10px"}}>
-                  <img src={item.pic02} alt="" style={{width:"56px",height:"56px",marginBottom:"10px"}}/>
-                  <p style={{fontSize: "16px",fontWeight:"300",}}>{item.title}</p>
-                  </div>
-                  {/* <img src={item.pic} alt=""  style={{width:"56px",height:"56px",marginTop: "5px",
-   marginLeft:"15px"}}/>
-                  <p style={{fontSize: "16px",fontWeight:"300",}}>{item.title}</p> */}
-
->>>>>>> 3e8dd8b890de97bff5bf6373da6405869dbd313d
                   </div>
 
 
@@ -180,141 +162,3 @@ const TabExample = props => {
 }
 
 export default HomeClassify
-<<<<<<< HEAD
-=======
-
-//  {
-/* <div>
-        <img className="bigimage" src="https://img02.hua.com/slider/19_day1111_all.jpg" alt=""/>
-     <nav className="catebox-details-list ">
-        <div className="catebox-details-item">
-
-        <img className="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Lover.png" alt="送恋人鲜花"/>
-        <p className="catebox-word">爱情鲜花</p>
-        </div>
-        <div className="catebox-details-item" >
-
-        <img className="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Elder.png" alt="送长辈鲜花"/>
-        <p className="catebox-word">长辈鲜花</p>
-        </div>
-        <div className="catebox-details-item" >
-
-        <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Birthday.png" alt="生日鲜花"/>
-        <p className="catebox-word">生日鲜花</p>
-        </div>
-
-    </nav>
-   <section>
-        <div className="catebox-details-item">
-
-        <img className="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Boxedflowers.png" alt="礼盒鲜花"/>
-              <p className="catebox-word">礼盒鲜花</p>
-        </div>
-        <div className="catebox-details-item">
-
-        <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_PPF.png" alt="永生花"/>
-              <p className="catebox-word" style={{textAlign:"center"}}>永生花</p>
-   </div>
-   <div className="catebox-details-item">
-
-       <img className="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Lover.png" alt="送恋人鲜花"/>
-       <p className="catebox-word" style={{textAlign:"center"}}>蛋糕</p>
-    </div>
-
-   </section>
-   <article>
-   <div className="catebox-details-item">
-
-   <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Cake_ganso.png" alt="元祖蛋糕"/>
-            <p className="catebox-word">元祖蛋糕</p>
-  </div>
-
-    <div className="catebox-details-item">
-
-    <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Goldfoilflower.png" alt="金箔花"/>
-    <p className="catebox-word" style={{textAlign:"center"}}>金箔花</p>
-    </div>
-
-   <div className="catebox-details-item">
-
-   <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Jewelry.png" alt="首饰"/>
-    <p className="catebox-word" style={{textAlign:"center"}}>首饰</p>
-</div>
-
-   </article>
-  <aside>
-   <div className="catebox-details-item">
-
-   <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Chocolate_qiaoluo.png" alt="巧罗巧克力"/>
-            <p className="catebox-word">巧罗克力</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_CreativeGifts.png" alt="创意礼品"/>
-            <p className="catebox-word" style={{textAlign:"center"}}>创意礼品</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Groupbuy.png" alt="企业团购"/>
-      <p className="catebox-word" style={{textAlign:"center"}}>企业团购</p>
-</div>
-
-   </aside>
-   <aside>
-   <div className="catebox-details-item">
-
-   <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Chocolate_qiaoluo.png" alt="巧罗巧克力"/>
-            <p className="catebox-word">巧罗克力</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_CreativeGifts.png" alt="创意礼品"/>
-            <p className="catebox-word" style={{textAlign:"center"}}>创意礼品</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Groupbuy.png" alt="企业团购"/>
-      <p className="catebox-word" style={{textAlign:"center"}}>企业团购</p>
-</div>
-
-   </aside>
-   <aside>
-   <div className="catebox-details-item">
-
-   <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Chocolate_qiaoluo.png" alt="巧罗巧克力"/>
-            <p className="catebox-word">巧罗克力</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_CreativeGifts.png" alt="创意礼品"/>
-            <p className="catebox-word" style={{textAlign:"center"}}>创意礼品</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Groupbuy.png" alt="企业团购"/>
-      <p className="catebox-word" style={{textAlign:"center"}}>企业团购</p>
-</div>
-
-   </aside>
-   <aside>
-   <div className="catebox-details-item">
-
-   <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Chocolate_qiaoluo.png" alt="巧罗巧克力"/>
-            <p className="catebox-word">巧罗克力</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_CreativeGifts.png" alt="创意礼品"/>
-            <p className="catebox-word" style={{textAlign:"center"}}>创意礼品</p>
-      </div>
-      <div className="catebox-details-item">
-
-      <img class="catebox-details-item-pic" src="https://img02.hua.com/m/category/Classification/m_category_hot_Groupbuy.png" alt="企业团购"/>
-      <p className="catebox-word" style={{textAlign:"center"}}>企业团购</p>
-</div>
-
-   </aside>
-
-   </div>  */
-// }
->>>>>>> 3e8dd8b890de97bff5bf6373da6405869dbd313d
